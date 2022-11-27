98521af2204c3b34cc19add29b86af6523ab508c
alexgicimo <gakurualexgicimo@gmail.com>
2022-11-27 21:14:28 +0300

    Rails router params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,26 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+   
   end
+
+  def grades
+    student_grades = Student.all.sort_by{|student| student["grade"]}
+
+    render json: student_grades
+end
+
+def highest_grade
+  top_scorer =  Student.all.sort_by{|student| student["grade"]}.last
+
+  render json: top_scorer
+end
+
+
+def show
+  student_show=Student.find(params[:id])
+
+  render json: student_show
+end
 
 end
